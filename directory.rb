68c4379ca679964f87f0162ae7ed45706b44afe1
Will <wadyoung@outlook.com>
2021-06-14 19:32:52 +0000

    Array refactor

--- directory.rb
+++ directory.rb
@@ -1,17 +1,24 @@
-# Print the list of students
-student_count = 11
+# Create an array of students
+students = [
+ "Dr. Hannibal Lecter",
+ "Darth Vader",
+ "Nurse Rached",
+ "Micheal Corleone",
+ "Alex DeLarge",
+ "The Wicked Witch of The West",
+ "Terminator",
+ "Freddy Kreuger",
+ "The Joker",
+ "Jeoffrey Baratheon",
+ "Norman Bates",
+]
+
+# puts title
 puts "The students of Villans Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Rached"
-puts "Micheal Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of The West"
-puts "Terminator"
-puts "Freddy Kreuger"
-puts "The Joker"
-puts "Jeoffrey Baratheon"
-puts "Norman Bates"
-# Work out the total amount of students
-puts "Overall, we have #{student_count} great students"
+
+# Iterate through and print the list of students
+students.each { |student| puts student }
+
+# puts a string using string interpolation 
+puts "Overall, we have #{students.count} great students"
