9821a6d50f2d0df78d6c98c61df222bc458ac46b
Will <wadyoung@outlook.com>
2021-06-17 13:02:01 +0000

    Added csv file

--- directory.rb
+++ directory.rb
@@ -57,6 +57,16 @@ def process(selection)
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line - student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
   print_menu
