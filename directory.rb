2e1a504af7a1b62c13b72193dc190f99a7b90d4c
Will <wadyoung@outlook.com>
2021-06-17 13:20:09 +0000

    Updated save method

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. List students"
+  puts "3. Save students to csv file"
   puts "9. Exit"
 end  
 
@@ -50,6 +51,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -61,7 +64,7 @@ def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line - student_data.join(",")
+    csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
